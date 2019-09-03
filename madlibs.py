8a4d5360fd2565659380a514fb220b3f8d3f36c6
uyennguyen16900 <uyennguyen16900@gmail.com>
2019-09-02 18:28:13 -0700

    Use a dictionary called template_vars and use .get(key) to find the input

--- madlibs.py
+++ madlibs.py
@@ -9,25 +9,42 @@ def madlibs():
 # For more information about sending data from a textbox into Flask, I read https://stackoverflow.com/questions/12277933/send-data-from-a-textbox-into-flask
 @app.route('/', methods=['POST'])
 def generate_story():
-    adj1 = request.form['adj1']
-    nationality = request.form['nationality']
-    person = request.form['person']
-    adverb = request.form['adverb']
-    noun1 = request.form['noun1']
-    adj2 = request.form['adj2']
-    noun2 = request.form['noun2']
-    adj3 = request.form['adj3']
-    adj4 = request.form['adj4']
-    plural_noun = request.form['plural_noun']
-    noun3 = request.form['noun3']
-    number1 = request.form['number1']
-    shapes = request.form['shapes']
-    food1 = request.form['food1']
-    food2 = request.form['food2']
-    verb = request.form['verb']
-    number2 = request.form['number2']
-
-    return render_template('story.html', adj1=adj1, nationality=nationality, person=person, adverb=adverb, noun1=noun1, adj2=adj2, noun2=noun2, adj3=adj3, adj4=adj4, plural_noun=plural_noun, noun3=noun3, number1=number1, shapes=shapes, food1=food1, food2=food2, verb=verb, number2=number2)
+    template_vars = {
+        'adj1': request.form['adj1'],
+        'nationality': request.form['nationality'],
+        'person': request.form['person'],
+        'adverb': request.form['adverb'],
+        'noun1': request.form['noun1'],
+        'adj2': request.form['adj2'],
+        'noun2': request.form['noun2'],
+        'adj3': request.form['adj3'],
+        'adj4': request.form['adj4'],
+        'plural_noun': request.form['plural_noun'],
+        'noun3': request.form['noun3'],
+        'number1': request.form['number1'],
+        'shapes': request.form['shapes'],
+        'food1': request.form['food1'],
+        'food2': request.form['food2'],
+        'verb': request.form['verb'],
+        'number2': request.form['number2'],
+    }
+    return render_template('story.html', adj1=template_vars.get('adj1'),
+                                        nationality=template_vars.get('nationality'),
+                                        person=template_vars.get('person'),
+                                        adverb=template_vars.get('adverb'),
+                                        noun1=template_vars.get('noun1'),
+                                        adj2=template_vars.get('adj2'),
+                                        noun2=template_vars.get('noun2'),
+                                        adj3=template_vars.get('adj3'),
+                                        adj4=template_vars.get('adj4'),
+                                        plural_noun=template_vars.get('plural_noun'),
+                                        noun3=template_vars.get('noun3'),
+                                        number1=template_vars.get('number1'),
+                                        shapes=template_vars.get('shapes'),
+                                        food1=template_vars.get('food1'),
+                                        food2=template_vars.get('food2'),
+                                        verb=template_vars.get('verb'),
+                                        number2=template_vars.get('number2'))
 
 if __name__ == '__main__ ':
     app.run(debug = True)
